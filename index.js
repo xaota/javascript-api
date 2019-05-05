6e017ddbcf2c841ee7ba63cb1c94da04b19cf63a
xaota <xaota@users.noreply.github.com>
2019-05-05 21:49:24 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const wrap = {
+  'application/json': data => JSON.stringify(data),
+  'multipart/form-data': data => {
+    const formData = new FormData();
+    Object.keys(data).forEach(name => formData.append(name, data[name]));
+    return formData;
+  }
+}
+
+/** {Api} работа с api @class
+  *
+  */
+  export default class Api {
+  /** Создание Api @constructor
+    * @param {string} endpoint точка входа
+    */
+    constructor(endpoint) {
+      this.endpoint = endpoint;
+    }
+
+  /** */
+    json(method, data, version = '1.0.0') {
+      return this.fetch(method, data, {version, type: 'application/json'});
+    }
+
+  /** */
+    file(method, data, version = '1.0.0') {
+      return this.fetch(method, data, {version, type: 'multipart/form-data'});
+    }
+
+  /** */
+    async fetch(method, data, params) {
+      const url = this.endpoint + '?' + method; // + &version=params.version
+      const body = wrap[params.type](data);
+      const options = {
+        method: 'post',
+        headers: {
+          "Content-type": params.type + "; charset=UTF-8"
+        },
+        body
+      };
+      const response = await fetch(url, options);
+      return response.json();
+    }
+  }
